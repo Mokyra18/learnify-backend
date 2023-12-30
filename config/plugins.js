f9b81a8277223a60b6d934b4c85ab927f04a987f
Mokyra18 <rizkyramadhani181102@gmail.com>
2023-12-30 16:22:36 +0700

    first commit

---
+++ config/plugins.js
@@ -0,0 +1,19 @@
+module.exports = ({ env }) => ({
+  // ...
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: 'dzqmbc3vx',
+        api_key: '871789467874979',
+        api_secret: 'ABazSLcHQ94SFEqGe7PwewmAIw8',
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+  // ...
+});
