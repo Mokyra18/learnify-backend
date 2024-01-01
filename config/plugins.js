3cb26f7954f3d981c1924baf17647d8b2eec5303
Mokyra18 <rizkyramadhani181102@gmail.com>
2024-01-02 00:52:51 +0700

    first commit

--- config/plugins.js
+++ config/plugins.js
@@ -1,5 +1,16 @@
 module.exports = ({ env }) => ({
   // ...
+  "vercel-deploy": {
+    enabled: true,
+    config: {
+      deployHook: process.env.VERCEL_DEPLOY_PLUGIN_HOOK,
+      apiToken: process.env.VERCEL_DEPLOY_PLUGIN_API_TOKEN,
+      appFilter: process.env.VERCEL_DEPLOY_PLUGIN_APP_FILTER,
+      teamFilter: process.env.VERCEL_DEPLOY_PLUGIN_TEAM_FILTER,
+      // teamFilter: "your-team-id-on-vercel",
+      roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
+    },
+  },
   upload: {
     config: {
       provider: 'cloudinary',
